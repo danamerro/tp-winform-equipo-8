d8b8d1a4b89c58263ac2505785da5eac15296cbc
Dana Milena Erro <danamerro@gmail.com>
2026-09-09 13:58:35 -0300

    ajuste de clases de dominio

--- TPWinForm_equipo-8/dominio/Articulo.cs
+++ TPWinForm_equipo-8/dominio/Articulo.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace dominio
 {
-    internal class Articulo
+    public class Articulo
     {
         public int Id { get; set; }
         public string Codigo { get; set; }
