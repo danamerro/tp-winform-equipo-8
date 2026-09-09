fa527ba21268bd2e5531b0e202578e3c923d4592
Dana Milena Erro <danamerro@gmail.com>
2026-09-09 17:27:28 -0300

    se elimina espacio

--- TPWinForm_equipo-8/dominio/Articulo.cs
+++ TPWinForm_equipo-8/dominio/Articulo.cs
@@ -12,7 +12,6 @@ namespace dominio
         public Marca Marca { get; set; }
         public Categoria Categoria { get; set; }
         public decimal Precio { get; set; }
-
         public List<Imagen> Imagenes { get; set; }
     }
 }
